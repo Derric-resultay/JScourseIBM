883c88dc670bac4f51194a509dbccac2351ec811
Derric-resultay <derricbresultay@gmail.com>
2024-09-06 23:05:17 -0400

    added prices to the items

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - 12.22$', 'Eggs Benedict - 5.20$', 'Oatmeal - 10.5$', 'Frittata - 5$'];
+const mainCourseMenu = ['Steak - 50$', 'Pasta - 30$', 'Burger - 25$', 'Salmon - 45$'];
+const dessertMenu = ['Cake - 20$', 'Ice Cream - 10$', 'Pudding - 10$', 'Fruit Salad - 15$'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
