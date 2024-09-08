f06fa58a578a18da83ea89fef3f9ca5d8df8ce74
Derric-resultay <derricbresultay@gmail.com>
2024-09-07 21:45:49 -0400

    added a delete button

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -27,6 +27,7 @@ function showbooks() {
     <p><strong>Author Name: </strong>${book.authorName}</p>
     <p><strong>Book Description: </strong>${book.bookDescription}</p>
     <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>
+    <button onclick="deletebook(${index})">Delete</button>
     `);
 
     document.getElementById('books').innerHTML = booksDiv.join('');
@@ -37,4 +38,10 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deletebook(index) {
+    books.splice(index, 1);
+
+    showbooks();
 }
