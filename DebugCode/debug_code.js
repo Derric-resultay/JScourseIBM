c8aa5e55d6296bc676396263a24c6e58d9bb5f78
Derric-resultay <derricbresultay@gmail.com>
2024-09-09 08:39:06 -0400

    added other operations

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,12 +1,25 @@
+function checking(op){
+    if (op !== "+" && op !== "-" && op !== "*" && op !== "/"){
+        return true;
+    }
+}
 function performOperation(){
     // Get user input from input fields
     let num1 = parseInt(document.getElementById("input1").value);
     let num2 = parseInt(document.getElementById("input2").value);
+    let op = document.getElementById("input3");
 
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)){
-        // Perform the operation
-        let result = multiply(num1, num2);
+        let result;
+        // Validate the operator used
+        if (checking(op.value)) {
+            alert("Please input a valid operator: (+, -, *, /)");
+            op.value = '';
+            return;
+        } else {
+            result = performOps(num1, num2, op.value);
+        }
 
         // Display the result
         displayResult(result);
@@ -15,12 +28,25 @@ function performOperation(){
     }
 }
 
-function multiply(a, b){
+function performOps(a, b, op){
     // Introduce a debugger statement to pause execution
     debugger;
-
+    let resultOps;
+    if (op === "+") {
+            resultOps = a + b;
+    }   else if (op === "-") {
+            resultOps = a - b;
+    }
     // Multiply the numbers
-    return a * b;
+        else if (op === "*"){
+            resultOps = a * b;
+    }   else if (op === "/") {
+            resultOps = a / b;
+    } else {
+        alert("Error");
+    }
+    return resultOps;
+
 }
 
 function displayResult(result){
